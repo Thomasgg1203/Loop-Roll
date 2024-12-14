7db2086eaa08d4e61f9168aa86ca37c8487b1131
Thomasgg <thomasgg1203@gmail.com>
2024-12-14 16:24:32 -0500

    cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <h1>HolA MUNDO</h1>
+    <h1>Hola com esta mundo?</h1>
   )
 }
 
